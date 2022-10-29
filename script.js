48c1832890db7504fed56aa3e06c1dd752d048e8
Fares Qaddoura <frqd92@gmail.com>
2022-10-29 12:52:23 +0100

    add another card and start implementing some JS logic

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ navIcons.addEventListener("mouseover", ()=>{
         hoverNav.classList.remove("links-hover")
     })
 })
+window.onload = function selectDropdown(){
+    let graphTime = document.querySelector(".graph-time");
+    let graphCal = document.querySelector(".graph-calories");
+    let select = document.getElementById("select-weekly");
+    select.addEventListener("input", ()=>{
+        if(select.value === "time"){
+            graphTime.style.display="block";
+            graphCal.style.display="none";
+        }
+        else{
+            graphTime.style.display="none";
+            graphCal.style.display="block";
+        }
+    })
+
+}
