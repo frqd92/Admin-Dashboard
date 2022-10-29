88b928adc0a99124595b47ec81b9c7fc2a0aa2bf
Fares Qaddoura <frqd92@gmail.com>
2022-10-29 18:17:29 +0100

    add more cards

--- script.js
+++ script.js
@@ -20,5 +20,32 @@ window.onload = function selectDropdown(){
             graphCal.style.display="block";
         }
     })
-
 }
+
+
+let hideAnn = document.querySelector(".hide-ann");
+let showAnn = document.querySelector(".show-ann");
+let ann = document.querySelector(".announcements");
+let cardContainer = document.querySelector(".card-container");
+hideAnn.addEventListener("click", ()=>{
+    cardContainer.classList.add("card-container-hidden-ann");
+    ann.style.display="none";
+    showAnn.style.display="block";
+    showAnn.addEventListener("click", ()=>{
+        showAnn.style.display="none";
+        ann.style.display="grid";
+        cardContainer.classList.remove("card-container-hidden-ann");
+        
+    })
+})
+
+window.addEventListener("scroll", ()=>{
+    let links = document.querySelector(".links");
+    if(window.scrollY > 7){
+        links.style.borderRadius= "0px";
+    }
+    else{
+        links.style.borderRadius = "50px 0 0 0";
+    }
+})
+
